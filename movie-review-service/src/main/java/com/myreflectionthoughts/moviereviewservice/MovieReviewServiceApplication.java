1edf3696a80ea5d436d27a0f06ca7ea5ba131407
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-22 14:28:02 +0530

    initial setup

---
+++ movie-review-service/src/main/java/com/myreflectionthoughts/moviereviewservice/MovieReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.moviereviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewServiceApplication.class, args);
+	}
+
+}
---
+++ movie-review-service/src/test/java/com/myreflectionthoughts/moviereviewservice/MovieReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.moviereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
