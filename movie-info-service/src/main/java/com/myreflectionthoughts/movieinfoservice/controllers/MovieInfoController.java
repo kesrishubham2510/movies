8f0f0c69b7a0cdf2cd49a8ace33558dac6200a29
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-23 12:29:22 +0530

    * Added end-point to add movieInfo

---
+++ movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/controllers/MovieInfoController.java
@@ -0,0 +1,31 @@
+package com.myreflectionthoughts.movieinfoservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myreflectionthoughts.movieinfoservice.dto.request.AddMovieInfo;
+import com.myreflectionthoughts.movieinfoservice.dto.response.MovieInfoResponse;
+import com.myreflectionthoughts.movieinfoservice.services.MovieInfoService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("movie-info-service/")
+public class MovieInfoController {
+
+    @Autowired
+    private MovieInfoService movieInfoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<MovieInfoResponse> addMovieInfo(@RequestBody Mono<AddMovieInfo> addMovieInfo){
+        return movieInfoService.save(addMovieInfo);
+    }
+
+    
+}
