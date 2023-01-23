dd07bcac2216e80daf389f84d171dc829b565dc1
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-23 12:26:04 +0530

    * Added configurations for service in application.yml file. * Added properties to MovieInfo DTOs

--- movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/dto/request/AddMovieInfo.java
+++ movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/dto/request/AddMovieInfo.java
@@ -1,5 +1,15 @@
 package com.myreflectionthoughts.movieinfoservice.dto.request;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class AddMovieInfo {
-    
+
+    private String title;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate releaseDate;
 }
--- movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/dto/response/MovieInfoResponse.java
+++ movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/dto/response/MovieInfoResponse.java
@@ -1,6 +1,17 @@
 package com.myreflectionthoughts.movieinfoservice.dto.response;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class MovieInfoResponse {
     
+    private String movieInfoId;
+    private String title;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate releaseDate;
 }
 
