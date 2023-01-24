b8ad3502a7249ae7e54f5d2d888ef2a8c28fb27e
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-24 11:35:18 +0530

    Removed unused import

--- movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/controllers/MovieInfoController.java
+++ movie-info-service/src/main/java/com/myreflectionthoughts/movieinfoservice/controllers/MovieInfoController.java
@@ -2,7 +2,6 @@ package com.myreflectionthoughts.movieinfoservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
