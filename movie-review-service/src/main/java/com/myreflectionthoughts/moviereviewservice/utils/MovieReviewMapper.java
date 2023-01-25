afa35942930c0b3cdf8f10c01b57b822e13bf5ed
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-25 21:44:13 +0530

    Added a mapping utitity to help in DTO<-->model conversions for Review

---
+++ movie-review-service/src/main/java/com/myreflectionthoughts/moviereviewservice/utils/MovieReviewMapper.java
@@ -0,0 +1,29 @@
+package com.myreflectionthoughts.moviereviewservice.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.myreflectionthoughts.moviereviewservice.dto.request.AddReview;
+import com.myreflectionthoughts.moviereviewservice.dto.response.ReviewResponse;
+import com.myreflectionthoughts.moviereviewservice.models.Review;
+
+@Component
+public class MovieReviewMapper{
+    
+     public Review toEntity(AddReview reqDTO){
+         var  movieReview = new Review();   
+         movieReview.setRating(reqDTO.getRating());
+         movieReview.setReview(reqDTO.getReview());
+         movieReview.setMovieInfoId(reqDTO.getMovieInfoId());
+         return movieReview;
+     }
+
+     public ReviewResponse toReviewResponse(Review review){
+        var reviewResponse = new ReviewResponse();
+        reviewResponse.setMovieInfoId(review.getMovieInfoId());
+        reviewResponse.setReviewId(review.getReviewId());
+        reviewResponse.setRating(review.getRating());
+        reviewResponse.setReview(review.getReview());
+        return reviewResponse;
+     }
+    
+}
