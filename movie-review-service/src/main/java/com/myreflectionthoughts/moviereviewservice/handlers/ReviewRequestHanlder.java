f2051fa744b9dc48a3ab1c48a10a3a778a832b89
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-26 21:13:40 +0530

    * Added Router for mapping incoming requests to particular request handler. * Added Request handler containing methods to perform specific task for specific request.

---
+++ movie-review-service/src/main/java/com/myreflectionthoughts/moviereviewservice/handlers/ReviewRequestHanlder.java
@@ -0,0 +1,52 @@
+package com.myreflectionthoughts.moviereviewservice.handlers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.myreflectionthoughts.moviereviewservice.dto.request.AddReview;
+import com.myreflectionthoughts.moviereviewservice.dto.request.UpdateReview;
+import com.myreflectionthoughts.moviereviewservice.dto.response.DeleteReviewResponse;
+import com.myreflectionthoughts.moviereviewservice.dto.response.ReviewResponse;
+import com.myreflectionthoughts.moviereviewservice.services.ReviewService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ReviewRequestHanlder {
+
+    @Autowired
+    private ReviewService reviewService;
+    
+    public Mono<ServerResponse> handleAddReview(ServerRequest serverRequest){
+ 
+        Mono<ReviewResponse> response =reviewService.save(serverRequest.bodyToMono(AddReview.class));
+        return ServerResponse.status(HttpStatus.CREATED).body(response,ReviewResponse.class);
+    }
+
+    public Mono<ServerResponse> handleGetReview(ServerRequest serverRequest){
+
+         String reviewId = serverRequest.pathVariable("id");
+         Mono<ReviewResponse> response = reviewService.find(reviewId);
+         return ServerResponse.ok().body(response, ReviewResponse.class);
+    }
+
+    public Mono<ServerResponse> handleGetallReviews(ServerRequest serverRequest){
+        Flux<ReviewResponse> reviewsFlux = reviewService.getAll();
+        return ServerResponse.ok().body(reviewsFlux, ReviewResponse.class);
+    }
+
+    public Mono<ServerResponse> handleUpdateReview(ServerRequest serverRequest){
+        Mono<ReviewResponse> response = reviewService.updateEntity(serverRequest.bodyToMono(UpdateReview.class));
+        return ServerResponse.ok().body(response, ReviewResponse.class);
+    }
+
+    public Mono<ServerResponse> handleDeleteReview(ServerRequest serverRequest){
+        Mono<DeleteReviewResponse> response = reviewService.delete(serverRequest.pathVariable("id"));
+        return ServerResponse.ok().body(response, DeleteReviewResponse.class);
+    }
+
+}
---
+++ movie-review-service/src/main/java/com/myreflectionthoughts/moviereviewservice/routers/ReviewRouter.java
@@ -0,0 +1,29 @@
+package com.myreflectionthoughts.moviereviewservice.routers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.myreflectionthoughts.moviereviewservice.handlers.ReviewRequestHanlder;
+
+@Configuration
+public class ReviewRouter{
+
+    @Autowired
+    private ReviewRequestHanlder reviewRequestHanlder;
+
+    @Bean
+    public RouterFunction<ServerResponse> reviewsRoute(){
+         return RouterFunctions.route()
+                               .POST("/movie-review-service/review/",reviewRequestHanlder::handleAddReview)
+                               .GET("/movie-review-service/review/{id}", reviewRequestHanlder::handleGetReview)
+                               .GET("/movie-review-service/review/",reviewRequestHanlder::handleGetallReviews)
+                               .PUT("/movie-review-service/review/",reviewRequestHanlder::handleUpdateReview)
+                               .DELETE("/movie-review-service/review/{id}",reviewRequestHanlder::handleDeleteReview)
+                               .build();
+    }
+
+}
