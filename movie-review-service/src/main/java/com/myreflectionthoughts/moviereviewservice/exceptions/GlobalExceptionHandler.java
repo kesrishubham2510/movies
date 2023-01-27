ac26aacfec4884966d7ae1aa94c926d01e3582b3
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-27 10:51:22 +0530

    Implemented ErrorwebExceptionHandler.

---
+++ movie-review-service/src/main/java/com/myreflectionthoughts/moviereviewservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.myreflectionthoughts.moviereviewservice.exceptions;
+
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalExceptionHandler implements ErrorWebExceptionHandler{
+   
+   @Override
+   public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+      
+      DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
+      var errorMessage = dataBufferFactory.wrap(ex.getMessage().getBytes());
+
+      if(ex instanceof ReviewNotFoundException){
+         exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+         return exchange.getResponse().writeWith(Mono.just(errorMessage));
+      }
+
+      exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+      return exchange.getResponse().writeWith(Mono.just(errorMessage));
+   }
+}
