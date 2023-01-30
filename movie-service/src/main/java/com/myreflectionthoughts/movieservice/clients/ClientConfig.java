92e3c4a1ce5a8ad7d5ee717dcb2d8a9983b0023c
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-30 12:56:59 +0530

    removed print statements.

--- movie-service/src/main/java/com/myreflectionthoughts/movieservice/clients/ClientConfig.java
+++ movie-service/src/main/java/com/myreflectionthoughts/movieservice/clients/ClientConfig.java
@@ -16,13 +16,11 @@ public class ClientConfig {
 
     @Bean(name="movie-review-client")
     public WebClient webReviewClient(){
-       System.out.println("URL:- "+movieReviewServiceURL);
        return WebClient.create(movieReviewServiceURL);
     }
 
     @Bean(name="movie-info-client")
     public WebClient webInfoClient(){
-        System.out.println("URL:- "+movieInfoServiceURL);
         return WebClient.create(movieInfoServiceURL);
     }
 }
